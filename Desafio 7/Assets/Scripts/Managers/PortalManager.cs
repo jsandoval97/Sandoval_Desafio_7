65c9fc7d8c4d66c155d1ad2d61cfbef3af61bf48
jsandoval97 <88573821+jsandoval97@users.noreply.github.com>
2022-08-11 21:35:05 -0300

    Commit Inicial

---
+++ Desafio 7/Assets/Scripts/Managers/PortalManager.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PortalManager : MonoBehaviour
+{
+    public Transform playerTransform;
+    bool iAlreadyShrink = false;
+
+    [SerializeField]
+    [Range(2, 10)]
+    private float cooldown;
+    private float timeFromShrink = 0;
+    private void Update ()
+    {
+        //Debug.Log(timeFromShrink);
+        timeFromShrink += Time.deltaTime;
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("Entrando en -> " + other.gameObject.name);
+
+        if (other.gameObject.CompareTag("Player") && iAlreadyShrink == false)
+        {
+            playerTransform.localScale = playerTransform.localScale /2;
+            iAlreadyShrink = true;
+            timeFromShrink = 0;
+        }
+
+        if (other.gameObject.CompareTag("Player") && iAlreadyShrink == true && timeFromShrink>=cooldown)
+        {
+            playerTransform.localScale = playerTransform.localScale *2;
+            iAlreadyShrink = false;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        Debug.Log("Saliendo de -> " + other.gameObject.name);  
+        
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        Debug.Log("Dentro de -> " + other.gameObject.name);
+    }
+}
---
+++ Desafio 7/Assets/Scripts/Player/PlayerCollision.cs
@@ -0,0 +1,49 @@
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCollision : MonoBehaviour
+{
+   
+    private void OnCollisionEnter(Collision other)
+    {
+        
+        if(other.gameObject.CompareTag("Portal"))
+        {
+            Debug.Log ("Entrando en colisión con un Shrinker");
+        }
+        else 
+        {
+            Debug.Log("Entrando en colisión con -> " + other.gameObject.name);
+        }
+
+    }
+
+    private void OnCollisionExit(Collision other)
+    {
+        Debug.Log("Saliendo de colisión con -> " + other.gameObject.name);
+    }
+
+    private void OnCollisionStay(Collision other)
+    {
+        Debug.Log("En colisión con -> " + other.gameObject.name);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("Entrando en -> " + other.gameObject.name);
+
+
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        Debug.Log("Saliendo de -> " + other.gameObject.name);
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        Debug.Log("Dentro de -> " + other.gameObject.name);
+    }
+}
